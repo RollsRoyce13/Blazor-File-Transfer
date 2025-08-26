7defa337a15eb02e93835ac323353a85644288c4
RollsRoyce13 <106380187+RollsRoyce13@users.noreply.github.com>
2025-08-26 13:48:00 +0300

    Initial commit

---
+++ File_Upload/Services/FileUpload.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace File_Upload.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public Task UploadFile(IBrowserFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
